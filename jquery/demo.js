6d43a2bc93eb07b1f7cd8065c46b6b5965c02f61
EHIO <1289868863@qq.com>
2018-01-09 12:49:11 +0800

    js 练习

---
+++ jquery/demo.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("input[id='pg']").click(function() {
+        console.log('pg');
+    });
+
+    $("input[id='xj']").click(function() {
+        console.log('xj');
+    });
+});
